9c90409eb2f1a77847e38efde00e1e9e02dfb517
Julio Cesar <julio.cesar_3@hotmail.com>
2024-02-27 01:48:00 +0000

    correções em toggle

--- src/js/script.js
+++ src/js/script.js
@@ -7,5 +7,19 @@ function openModal(){
       modal.classList.remove('open')
     }
   } )
-
 }
+
+  document.getElementById('toggle').addEventListener('change', function() {
+    document.querySelector('.menu').style.display = this.checked ? 'flex' : 'none';
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var toggle = document.getElementById("toggle");
+    var menuLinks = document.querySelectorAll(".menu a");
+
+    menuLinks.forEach(function(link) {
+      link.addEventListener("click", function() {
+        toggle.checked = false;
+      });
+    });
+  });
